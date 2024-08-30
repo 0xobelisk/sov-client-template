c8b032eb7d11b0d85b9ea0e605231a8cbdcde636
henryliu <17600047539@163.com>
2024-08-30 08:28:27 +0800

    update

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,49 +1,8 @@
 /** @type {import('next').NextConfig} */
 
-// const nextConfig = {
-//   output: 'export',
-//
-//   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-//   trailingSlash: true,
-//
-//   // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
-//   // skipTrailingSlashRedirect: true,
-//
-//   // Optional: Change the output directory `out` -> `dist`
-//   distDir: 'dist',
-// }
-
-// const nextConfig = {
-//   webpack: (config, { isServer }) => {
-//     // 添加对 .wasm 文件的支持
-//     config.experiments = {
-//       asyncWebAssembly: true,
-//       syncWebAssembly: true,
-//       layers: true,
-//     };
-
-//     config.module.rules.push({
-//       test: /\.wasm$/,
-//       type: 'webassembly/async',
-//     });
-
-//     return config;
-//   },
-// };
-
-
-// const nextConfig = {
-//   webpack: (config) => {
-//       config.experiments = {
-//           asyncWebAssembly: true,
-//           syncWebAssembly: true,
-//           layers: true,
-//           outputModule: true,
-//       };
-//       return config;
-//   }
-// };
 const nextConfig = {
+  // output: 'export',
+  // distDir: 'dist',
   async rewrites() {
     return [
       {
@@ -52,8 +11,7 @@ const nextConfig = {
       },
     ]
   },
-};
-
+}
 
 
 module.exports = {
